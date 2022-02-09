c8c4fef39c95bf4bbdf3dbcfb416ef86d90faac5
enriquepalaciosb <enriquegpalacios@gmail.com>
2022-02-09 11:08:12 -0800

    Added themepicker and altered JS to collect objects

---
+++ main.js
@@ -0,0 +1,21 @@
+// PAGE 2 JS
+// Hero Array
+let herosArray = [];
+// Creating a construtor to store in Array
+let HeroObject = function (pHeroName, pUserReport, pUserPhase, pURL) {
+    this.title = pHeroName;
+    this.report = pUserReport;
+    this.year = pUserPhase;
+    this.URL = pURL;
+}
+    document.addEventListener("DOMContentLoaded", function (event) {
+        document.getElementById("buttonAdd").addEventListener("click", function () {
+            herosArray.push(new HeroObject(document.getElementById("hero").value, document.getElementById("damage-report").value, document.getElementById("select-type").value, document.getElementById("footage").value));
+            // Uncomment line below for testing purposes
+            console.log(herosArray);
+            document.getElementById("hero").value = "";
+            document.getElementById("damage-report").value = "";
+            document.getElementById("select-type").value = "";
+            document.getElementById("footage").value = "";
+        });
+    });
