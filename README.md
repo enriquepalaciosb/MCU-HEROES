2482c08b5681c67497978ec2a197c528a6e82850
enriquepalaciosb <enriquegpalacios@gmail.com>
2022-02-23 14:06:29 -0800

    Cleared app.js, added neccessary npm's, and left readme blank

--- README.md
+++ README.md
@@ -11,31 +11,31 @@
 
 
 ## General Information
-- A name generator that will give you a random superhero or villain name.
+- 
 
 ## Technologies Used
 Project is created with:
 * Node.JS
 * JQuery
+* HTML/CSS/JS
+* NPM
 
 ## Features
-- Can give you both a random superhero and villain name.
+- 
 
 ## Usage
-Demonstrate simple tasks by utilizng Node and Express.
-Please check the package for neccessary installs.
+
 
 ## Project Status
 Project is: _in progress_ 
 
 ## Room for Improvement
 Room for improvement:
-- Moving it from console to a page.
+- 
 
 To do:
-- Create a HTML/CSS file.
+- 
 
 ## Acknowledgements
-- This project was based on [these materials](https://www.udemy.com/course/the-complete-web-development-bootcamp/).
-- Many thanks to The App Brewery
+- 
 
--- app.js
+++ app.js
@@ -5,23 +5,9 @@ app.set('view engine', 'ejs')
 
 app.use( express.static( "public" ) );
 
-const movies = [
-    {title: 'Wild At Heart', year: '1993' },
-    {title: 'Moonstruck', year: '1994' },
-    {title: 'Raising Arizona', year: '1995' }
-]
+const heroesArray = [
 
-const posts = [
-    {title: 'Title 1', body: 'Body 1' },
-    {title: 'Title 2', body: 'Body 2' },
-    {title: 'Title 3', body: 'Body 3' },
-    {title: 'Title 4', body: 'Body 4' },
 ]
-const user = {
-    firstName: 'Good',
-    lastName: 'Student',
-    admin: true
-}
 
 app.get('/', (req, res) => {
     res.render('pages/index', {
@@ -46,6 +32,13 @@ app.get('/articles', (req, res) => {
         articles: posts
     })
 })
+
+app.get('/articles', (req, res) => {
+    res.render('pages/articles', {
+        articles: posts
+    })
+})
+
 app.listen(port, () => {
   console.log(`App listening at port ${port}`)
 })
